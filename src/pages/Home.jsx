242bdfc07961b42d44832d28cebc1adba2a63bf7
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2023-10-07 16:19:03 +0530

    encode X digitalidentity

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import './home.css'
+const Home = ()=>{
+    return(
+    <div className='home__container'>
+      <h1 className='heading__sec first'>Enjoy minting NFT with Zero Penny!!</h1>
+      <p className='para'>
+        We offer you to mint any Non Fungible Token from the Set   
+        <br/><br/>With Zero gas fee<br/>
+        <br/>Thanks to Biconomy!!<br/>
+      </p>
+      </div>
+    );
+};
+
+export default Home;
