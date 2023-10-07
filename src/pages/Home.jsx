6c811474a51132493fe896c5e58125165505ceed
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2023-10-07 16:51:09 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import './home.css'
 const Home = ()=>{
     return(
     <div className='home__container'>
-      <h1 className='heading__sec first'>Enjoy minting NFT with Zero Penny!!</h1>
+      <h1 className='heading__sec first'>Empowering Artists,Fee-Free Green Minting.!!</h1>
       <p className='para'>
-        We offer you to mint any Non Fungible Token from the Set   
-        <br/><br/>With Zero gas fee<br/>
+        Revolutionizing NFTs: Minting Masterpieces, Zero Gas Fees.   
+        <br/><br/>"MintEase"<br/>
         <br/>Thanks to Biconomy!!<br/>
+        <img src='NoFuel.png' style={{width:100, marginTop:10}}></img>
       </p>
       </div>
     );
