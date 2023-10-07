816435b0ec651c82e238c6488d3c4872c1054fd2
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2023-10-07 16:57:07 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import './home.css'
 const Home = ()=>{
     return(
     <div className='home__container'>
-      <h1 className='heading__sec first'>Empowering Artists,Fee-Free Green Minting.!!</h1>
+      <h1 className='heading__sec first'>Empowering Artists, Fee-Free Green Minting.!!</h1>
       <p className='para'>
         Revolutionizing NFTs: Minting Masterpieces, Zero Gas Fees.   
         <br/><br/>"MintEase"<br/>
         <br/>Thanks to Biconomy!!<br/>
-        <img src='NoFuel.png' style={{width:100, marginTop:10}}></img>
+        <br></br>
+        <img src='NoFuel.png' style={{width:300, marginTop:20}}></img>
       </p>
       </div>
     );
